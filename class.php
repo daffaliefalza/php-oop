ca6de38ad3da4cf8da742c899b121b6325a75fe4
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2024-02-27 05:13:21 +0700

    add: class source code

---
+++ class.php
@@ -0,0 +1,37 @@
+<?php
+
+
+class User
+{
+
+
+    // properties 
+
+    // methods
+}
+
+// instance object
+// $userOne = new User();
+// $userTwo = new User();
+
+echo "The class is: " . get_class($userOne) . "<br>";
+echo "The class is: " . get_class($userTwo);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
